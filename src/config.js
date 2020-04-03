f64e03f15a36a9d6e3f4708fb61e054535f418e8
Inposa <donhippo04@gmail.com>
2020-04-03 16:00:48 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "upload-files-notes"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://4hjdzjgqxi.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_Tn9rY6Nso",
+      APP_CLIENT_ID: "23t881q9rs3ddo6t6sm0ejm3or",
+      IDENTITY_POOL_ID: "us-east-1:a6158478-ba3d-4775-8dea-56a0e4f5a698"
+    }
+  };
